431b3c07684372e6002005e55dcaf1c6491ae9d7
SUBHADEEP MAITI <35770307+deepsubha@users.noreply.github.com>
2023-06-13 21:59:28 +0530

    Create 199. Binary Tree Right Side View.java

---
+++ LeetCode/199. Binary Tree Right Side View.java
@@ -0,0 +1,6 @@
+https://leetcode.com/problems/binary-tree-right-side-view/
+Right Side View of BST - need to check each level if no right node present need to use left node e.g. below:
+    1
+ 2    null     --> right view would be    [1,2]
+ 
+------------------------------------------------------------------------------------------------------------
